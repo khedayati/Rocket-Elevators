6394b9dda0ee1023891bb48832eb692d161ed185
Head0 <tsken010@outlook.com>
2022-01-14 18:38:05 -0500

    modified deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,8 @@ set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
-set :rvm_type , :system
+#set :rvm_type , :system
+set :rvm_custom_path, "/usr/share/rvm"
 
 ## Defaults:
 # set :scm,           :git
