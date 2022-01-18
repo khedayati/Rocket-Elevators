75aeca9f59960e51149f09d1cfbe5056fa5e8e24
Head0 <tsken010@outlook.com>
2022-01-17 21:31:02 -0500

    update

--- app/assets/javascripts/analytics.js
+++ app/assets/javascripts/analytics.js
@@ -3,7 +3,7 @@ function gtag() { dataLayer.push(arguments) }
 
 gtag('js', new Date())
 
-const trackGoogleAnalytics = (event) => {
+var trackGoogleAnalytics = (event) => {
   gtag('config', 'GA_MEASUREMENT_ID', {
     'cookie_flags': 'max-age=7200;secure;samesite=none'
   })
